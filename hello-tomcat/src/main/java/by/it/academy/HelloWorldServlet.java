cd6c525fe86c4aee115f8b6b75f4e70ebd3adb23
Aliaksandr Veramkovich <averamkovich@allot.com>
2020-07-15 23:44:15 +0300

    Lesson 9: Hello Tomcat!

--- hello-tomcat/src/main/java/by/it/academy/HelloWorldServlet.java
+++ hello-tomcat/src/main/java/by/it/academy/HelloWorldServlet.java
@@ -1,6 +1,5 @@
 package by.it.academy;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -9,34 +8,41 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Calendar;
 import java.util.Enumeration;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
-// http://localhost:8080/hello-tomcat/echo
 @WebServlet(value = "/echo", name = "hello-world")
 public class HelloWorldServlet extends HttpServlet {
 
     private static final Logger log = Logger.getLogger("HelloWorldServlet");
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String contextPath = req.getContextPath();
-        log.info("Context path: " + contextPath);
-        System.out.println("Context path: " + contextPath);
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            String contextPath = req.getContextPath();
+            log.info(() -> String.format("Context path: %s", contextPath) );
 
-        PrintWriter writer = resp.getWriter();
-        writer.print(contextPath + ": Hello from my servlet" + Calendar.getInstance());
+            PrintWriter writer = resp.getWriter();
+            writer.print(contextPath + ": Hello from my servlet" + Calendar.getInstance());
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
+        }
 
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        Enumeration<String> parameterNames = req.getParameterNames();
-        resp.setContentType("text/html");
-        while (parameterNames.hasMoreElements()) {
-            String element = parameterNames.nextElement();
-            writer.println("<h3>" + element + ": " + req.getParameter(element) + "</h3>");
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+        try {
+            PrintWriter writer = resp.getWriter();
+            Enumeration<String> parameterNames = req.getParameterNames();
+            resp.setContentType("text/html");
+            while (parameterNames.hasMoreElements()) {
+                String element = parameterNames.nextElement();
+                writer.println("<h3>" + element + ": " + req.getParameter(element) + "</h3>");
+            }
+            writer.println("<a href='/hello-tomcat'>Back</a>");
+        } catch (IOException e) {
+            log.log(Level.SEVERE, e.getMessage(), e);
         }
-        writer.println("<a href='/hello-tomcat'>Back</a>");
     }
 }
