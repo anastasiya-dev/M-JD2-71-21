53e161ff358c8439a7ef532593addeed4eb479d6
Aliaksandr Veramkovich <averamkovich@allot.com>
2020-09-09 15:29:35 +0300

    Lesson 26: Spring Boot Example

---
+++ hello-spring-boot/hello-data-rest/src/main/java/by/it/academy/data/jpa/SampleDataRestApplication.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012-2014 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package by.it.academy.data.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@Import(RepositoryRestMvcConfiguration.class)
+public class SampleDataRestApplication {
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SampleDataRestApplication.class, args);
+	}
+}
